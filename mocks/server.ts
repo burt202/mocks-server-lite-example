74465a13e612ad1dc05d3752a6bdb934b32a519f
Aaron Burtnyk <aaron.burtnyk@candide.com>
2025-03-17 12:31:41 +0000

    Bump mocks-server-lite to v0.12.0

--- mocks/server.ts
+++ mocks/server.ts
@@ -5,10 +5,17 @@ import routes from "./routes"
 import staticPaths from "./staticPaths"
 import webSockets from "./webSockets"
 
-const server = createServer({
-  delay: 1000,
-  selected: process.env.SELECTED_MOCKS_COLLECTION ?? "base",
-  port: 3101,
-})
-
-void server.start({routes, collections, webSockets, staticPaths})
+void createServer(
+  {routes, collections, webSockets, staticPaths},
+  {
+    delay: 1000,
+    selected: process.env.SELECTED_MOCKS_COLLECTION ?? "base",
+    port: 3101,
+  },
+)
+  .then((server) => {
+    void server.start()
+  })
+  .catch(() => {
+    console.log("There was an error starting the server")
+  })
