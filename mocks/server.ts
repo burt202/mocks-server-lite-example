85cb4fb078a8978da16b3376bc16a932e3598bad
Aaron Burtnyk <aaron.burtnyk@candide.com>
2025-03-17 12:51:52 +0000

    Skip selection prompt in ci

--- mocks/server.ts
+++ mocks/server.ts
@@ -5,12 +5,15 @@ import routes from "./routes"
 import staticPaths from "./staticPaths"
 import webSockets from "./webSockets"
 
+const skipSelectionPrompt = process.env.MSL_SKIP_SELECTION_PROMPT === "true"
+
 void createServer(
   {routes, collections, webSockets, staticPaths},
   {
     delay: 1000,
     selected: process.env.SELECTED_MOCKS_COLLECTION ?? "base",
     port: 3101,
+    skipSelectionPrompt,
   },
 )
   .then((server) => {
